cf46df5fa0fd6138a1232f533180662b6b62047e
levicode <lzw19910408@gmail.com>
2014-12-11 13:52:41 +0800

    SqStack基本完成

---
+++ SqStack.cpp
@@ -0,0 +1,84 @@
+#include "SqStack.h"
+#include <iostream>
+using namespace std;
+
+SqStack::SqStack(int size)
+{
+	SIZE = size;  //使用size初始化顺序栈容量
+	sqstack = new ElemType [size];  //初始化顺序栈数组
+	top = -1;  //初始化栈顶指针
+}
+
+
+SqStack::~SqStack()
+{
+	delete [] sqstack;  //释放顺序栈元素数组内存
+	SIZE = 0;  //复位顺序栈容量
+}
+
+
+int SqStack::Length()
+{
+	return top + 1;  //栈顶指针加1则为顺序栈的长度
+}
+
+
+bool SqStack::Empty()
+{
+	if(top == -1)  //栈顶指针为-1时顺序栈为空
+		return true;
+	else
+		return false;
+}
+
+
+bool SqStack::Push(ElemType element)
+{
+	if(top + 1 == SIZE)  //顺序栈元素已满
+		return false;
+
+	top++;  //栈顶指针向上移动一位
+	sqstack[top] = element;  //在栈顶加入新的元素
+	return true;
+}
+
+
+bool SqStack::Pop(ElemType &element)
+{
+	if(top == -1)  //顺序栈为空
+		return false;
+
+	element = sqstack[top];  //将栈顶元素赋值给element
+	top--;  //栈顶指针向下移动一位
+	return true;
+}
+
+
+bool SqStack::GetTop(ElemType &element)
+{
+	if(top == -1)  //顺序栈为空
+		return false;
+
+	element == sqstack[top];  //将栈顶元素赋值给element
+	return true;
+}
+
+
+void SqStack::Clear()
+{
+	top == -1;  //栈顶指针置为-1
+}
+
+
+bool SqStack::Traversal()
+{
+	if(top == -1)  //顺序栈为空
+		return false;
+
+	int current = top;
+	while(current != -1)  //从栈顶开始遍历顺序栈输出元素值
+	{
+		cout<< sqstack[current--] << endl;  //输出完后current指针向下移动一位
+	}
+	return true;
+}
---
+++ SqStack.h
@@ -0,0 +1,21 @@
+#pragma once
+typedef int ElemType;  //顺序栈元素类型
+int SIZE = 0;  //顺序栈的容量
+
+class SqStack
+{
+public:
+	SqStack(int size);  //构造函数
+	~SqStack();  //析构函数
+	int Length();  //获取顺序栈的长度
+	bool Empty();  //判断顺序栈是否为空
+	bool Push(ElemType element);  //向栈中压入一个元素element
+	bool Pop(ElemType &element);  //从栈中取出一个元素返回给element
+	bool GetTop(ElemType &element);  //获取栈顶元素返回给element
+	void Clear();  //清空栈
+	bool Traversal();  //遍历栈
+private:
+	ElemType *sqstack;  //顺序栈元素数据
+	int top;  //栈顶指针
+};
+
